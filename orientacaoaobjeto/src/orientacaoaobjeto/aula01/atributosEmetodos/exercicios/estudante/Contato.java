a9b5133e95bd2fe7e9b5c26f4126ff5c0349fd42
osvaldo souza <osvaldo.souza.iwt@gmail.com>
2023-01-12 20:58:32 -0300

    primeiro commit]

---
+++ orientacaoaobjeto/src/orientacaoaobjeto/aula01/atributosEmetodos/exercicios/estudante/Contato.java
@@ -0,0 +1,9 @@
+package orientacaoaobjeto.aula01.atributosEmetodos.exercicios.estudante;
+
+public class Contato {
+
+	String telefone01;
+	String telefone02;
+	String email;
+	
+}
---
+++ orientacaoaobjeto/src/orientacaoaobjeto/aula01/atributosEmetodos/exercicios/estudante/Endereco.java
@@ -0,0 +1,11 @@
+package orientacaoaobjeto.aula01.atributosEmetodos.exercicios.estudante;
+
+public class Endereco {
+	
+	String logradouro;
+	String bairro;
+	String cep;
+	String municipio;
+	String estado;
+	String pais;
+}
