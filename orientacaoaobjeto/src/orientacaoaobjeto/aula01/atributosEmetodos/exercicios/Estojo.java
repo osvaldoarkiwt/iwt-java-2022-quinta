bab7834cd559bc43a95d8a5c6388034f72eed4be
pedrelosa <pedrelosa@hotmail.com>
2023-01-12 21:54:00 -0300

    feat- criando classe estojo

---
+++ orientacaoaobjeto/src/orientacaoaobjeto/aula01/atributosEmetodos/exercicios/Estojo.java
@@ -0,0 +1,12 @@
+package orientacaoaobjeto.aula01.atributosEmetodos.exercicios;
+
+public class Estojo {
+
+    String lapis;
+    String pincel;
+    String caneta;
+
+    public Estojo(String lapis) {
+        this.lapis = lapis;
+    }
+}
