8f813c5967ec8350e16fd77ee5776afdf31ad8fe
osvaldo arruda <osvaldo.souza.iwt@gmail.com>
2023-01-19 19:58:32 -0300

    fix - colocando o meu construtor no projeto

--- orientacaoaobjeto/src/orientacaoaobjeto/aula01/construtoresEEncapsulamento/Curso.java
+++ orientacaoaobjeto/src/orientacaoaobjeto/aula01/construtoresEEncapsulamento/Curso.java
@@ -9,7 +9,21 @@ public class Curso {
 	int cargaHoraria;
 	LocalDate comeco;
 	LocalDate fim;
+		
+	{
+		//this.fim = this.comeco.plusMonths(this.cargaHoraria /16);
+	}
 	
+	public Curso() {}
+	
+	public Curso(String nome, String turno, int cargaHoraria, LocalDate comeco) {
+		this.nome = nome;
+		this.turno = turno;
+		this.cargaHoraria = cargaHoraria;
+		this.comeco = comeco;
+		this.fim = this.comeco.plusMonths(this.cargaHoraria /16);
+	}
+
 	@Override
 	public String toString() {
 		return "Curso [nome=" + nome + ", turno=" + turno + ", cargaHoraria=" + cargaHoraria + ", comeco=" + comeco
--- orientacaoaobjeto/src/orientacaoaobjeto/aula01/construtoresEEncapsulamento/Endereco.java
+++ orientacaoaobjeto/src/orientacaoaobjeto/aula01/construtoresEEncapsulamento/Endereco.java
@@ -8,7 +8,16 @@ public class Endereco {
 	String municipio;
 	String estado;
 	String pais;
-	
+		
+	public Endereco(String logradouro, String bairro, String cep, String municipio, String estado, String pais) {
+		this.logradouro = logradouro;
+		this.bairro = bairro;
+		this.cep = cep;
+		this.municipio = municipio;
+		this.estado = estado;
+		this.pais = pais;
+	}
+
 	@Override
 	public String toString() {
 		return "Endereco [logradouro=" + logradouro + ", bairro=" + bairro + ", cep=" + cep + ", municipio=" + municipio
