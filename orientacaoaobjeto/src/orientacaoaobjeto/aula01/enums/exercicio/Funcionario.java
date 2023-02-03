0b50ff944ce35ebfdbd5e898e92b79ababf16054
osvaldo souza <osvaldo.souza.iwt@gmail.com>
2023-02-02 21:38:16 -0300

    docs: adicionando a explicação da tarefa de casa na classe funcionário

--- orientacaoaobjeto/src/orientacaoaobjeto/aula01/enums/exercicio/Funcionario.java
+++ orientacaoaobjeto/src/orientacaoaobjeto/aula01/enums/exercicio/Funcionario.java
@@ -1,10 +1,20 @@
 package orientacaoaobjeto.aula01.enums.exercicio;
 
+/*
+ * Classe funcionário 
+ * 
+ * autor: Osvaldo Souza
+ * 
+ * versão: 1.0
+ * 
+ * */
+//TODO criar uma interface que contenha os métodos de calculo
+//de salário através da frequência do funcionário.
 public class Funcionario {
 	
 	private String nome;
 	private VinculoEnum vinculo;
-	
+	//TODO modificar o construtor para receber o id do enum.
 	public Funcionario(String nome, VinculoEnum vinculo) {
 		this.nome = nome;
 		this.vinculo = vinculo;
@@ -21,7 +31,7 @@ public class Funcionario {
 	public String getVinculo() {
 		return vinculo.getDescricao();
 	}
-
+	//TODO modificar o método para receber o id do enum.
 	public void setVinculo(VinculoEnum vinculo) {
 		this.vinculo = vinculo;
 	}
